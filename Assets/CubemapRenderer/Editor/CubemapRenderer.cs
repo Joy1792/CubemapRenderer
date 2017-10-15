429ecf61cee6d41c04ee5cab241d845adfc14715
Mogoson <mogoson@qq.com>
2017-10-15 17:08:44 +0800

    Optimize scripts.

--- Assets/CubemapRenderer/Editor/CubemapRenderer.cs
+++ Assets/CubemapRenderer/Editor/CubemapRenderer.cs
@@ -1,20 +1,13 @@
 /*************************************************************************
  *  Copyright (C), 2017-2018, Mogoson Tech. Co., Ltd.
- *  FileName: CubemapRenderer.cs
- *  Author: Mogoson   Version: 0.1.0   Date: 8/31/2017
- *  Version Description:
- *    Internal develop version, mainly to achieve its function.
- *  File Description:
- *    Ignore.
- *  Class List:
- *    <ID>           <name>             <description>
- *     1.        CubemapRenderer           Ignore.
- *  Function List:
- *    <class ID>     <name>             <description>
- *     1.
- *  History:
- *    <ID>    <author>      <time>      <version>      <description>
- *     1.     Mogoson     8/31/2017       0.1.0        Create this file.
+ *------------------------------------------------------------------------
+ *  File         :  CubemapRenderer.cs
+ *  Description  :  Render a scene into a static Cubemap asset.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  8/31/2017
+ *  Description  :  Initial development version.
  *************************************************************************/
 
 using UnityEditor;
